494e9936f1dac3ffa64e8051b2ad4fd6ec10d773
alenxmedia <ahunjetxmedia@gmail.com>
2021-09-24 10:36:06 +0200

    Added example short code.

--- wp-test-plugin.php
+++ wp-test-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name:       WP test plugin
- * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handle the basics with this plugin.
+ * Plugin URI:        https://github.com/alenxmedia/wp-test-plugin
+ * Description:       Learning to develop WP plugin.
  * Version:           1.10.3
  * Requires at least: 5.2
  * Requires PHP:      7.2
@@ -14,4 +14,11 @@
  * Text Domain:       my-basics-plugin
  * Domain Path:       /languages
  */
+
+  function test_plugin_example_function()
+  {
+	  $information = "This is a test plugin.";
+	  return $information;
+	}
+  add_shortcode( "test_plugin_shorcode", 'test_plugin_example_function' );
 ?>
